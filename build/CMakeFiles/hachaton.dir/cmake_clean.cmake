0dbb8ea91a254ad40a11defbf5ef408844a02fd6
Eliott <eliott.barbier@mines-paristech.fr>
2022-01-28 09:37:06 +0100

    Crea

---
+++ build/CMakeFiles/hachaton.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hachaton.dir/main.cpp.o"
+  "hachaton"
+  "hachaton.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hachaton.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
